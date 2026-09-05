356f1ca72567f1b4e8295cbaf426188a610f0f87
Reid Smith <smithreid01@gmail.com>
2026-09-05 01:50:14 -0400

    Updated License and source files

--- src/classes/speech_recognizer/speech_recognizer.cpp
+++ src/classes/speech_recognizer/speech_recognizer.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 Reid Smith
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <godot_cpp/classes/json.hpp>
 #include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/classes/worker_thread_pool.hpp>
